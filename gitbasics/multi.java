333e4942c01104c82f018a61c5c31e6162628923
sonu kumar <sonu9030980375@gmail.com>
2022-12-03 14:32:59 +0100

    fourth commit

---
+++ gitbasics/multi.java
@@ -0,0 +1,14 @@
+public class JavaExample {
+    public static void main(String[] args) {
+        // two integer variables with values
+        // and a variable "sum" to store the result
+        int num1 = 5, num2 = 15, mul;
+
+        // calculating the sum of num1 and num2 and
+        // storing the result in the variable sum
+        mul = num1 * num2;
+
+        // printing the result
+        System.out.println("multi of " + num1 + " and " + num2 + " is: " + mul);
+    }
+}
