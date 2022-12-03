f1ab2daf32578ff58aae5f329704a1aa06bae1cc
sonu11kumar <sonu9030980375@gmail.com>
2022-12-03 16:05:59 +0100

    5th commit

--- gitbasics/add.java
+++ gitbasics/add.java
@@ -2,11 +2,11 @@ public class JavaExample {
     public static void main(String[] args) {
         // two integer variables with values
         // and a variable "sum" to store the result
-        int num1 = 5, num2 = 15, num3 = 4, sum;
+        int num1 = 5, num2 = 15, sum;
 
         // calculating the sum of num1 and num2 and
         // storing the result in the variable sum
-        sum = num1 + num2 + num3;
+        sum = num1 + num2;
 
         // printing the result
         System.out.println("Sum of " + num1 + " " + num1 + " and " + num2 + " is: " + sum);
